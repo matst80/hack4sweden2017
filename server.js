43b80f307583e8107d234e3a55c76d9ca16c5c19
Mats Törnberg <mats_t80@hotmail.com>
2017-03-11 09:45:15 +0100

    basic server

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+//var server = express.createServer();
+// express.createServer()  is deprecated. 
+var server = express(); // better instead
+server.configure(function() {
+    server.use('/media', express.static(__dirname + '/media'));
+    server.use(express.static(__dirname + '/public'));
+});
+
+server.listen(80);
