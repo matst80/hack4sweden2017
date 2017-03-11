fdaf39f1db04d18d153f4b7ecb2585253d5abc68
Per-Olov Jernberg <possan@possan.se>
2017-03-11 20:22:19 +0100

    Fix click thing

--- testapp-po/src/js/main.js
+++ testapp-po/src/js/main.js
@@ -48,8 +48,8 @@ var handler1 = new ol.interaction.Pointer({
   handleDownEvent: function(e) {
     var t = ol.proj.transform(e.coordinate, 'EPSG:3857', 'EPSG:4326')
     console.log('click on', e, t)
-    var p1 = getPixelFeatures(e.pixel[0], e.pixel[0]);
-    var p2 = getPixelFeatures(e.pixel[0] + 100, e.pixel[0]);
+    var p1 = getPixelFeatures(e.pixel[0], e.pixel[1]);
+    var p2 = getPixelFeatures(e.pixel[0] + 100, e.pixel[1]);
     p1.then(function(clean) {
       p2.then(function(clean2) {
         document.getElementById('overlay').innerText = JSON.stringify(clean, null, 2)
