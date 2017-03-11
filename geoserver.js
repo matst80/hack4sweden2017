d352bd2c0b84b022af3fc72b2ea8f5bec9a19fe8
Mats Törnberg <mats_t80@hotmail.com>
2017-03-11 20:26:58 +0100

    multiball

--- geoserver.js
+++ geoserver.js
@@ -2,13 +2,13 @@ var gju = require('geojson-utils');
 var geodata = require('./parsed_diff.json');
 
 function findData(lat, lng) {
-    return geodata.features.filter(function(v) {
+    var ret = [];
+    ret = geodata.features.filter(function(v) {
         return gju.pointInPolygon({ "type": "Point", "coordinates": [lat, 55.39] },
             v.geometry
         );
-
     });
-
+    return ret;
 }
 
 console.log(findData(13.034, 55.39));
