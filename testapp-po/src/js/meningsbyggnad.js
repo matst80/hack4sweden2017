3185001e67152001d640c7bd76d4df8d7cd94702
Marcus Kempe <makem86@gmail.com>
2017-03-12 01:47:39 +0100

    Meningsbyggnad

--- testapp-po/src/js/meningsbyggnad.js
+++ testapp-po/src/js/meningsbyggnad.js
@@ -4,25 +4,25 @@ var params = {
     "admin_level": "administratörsnivå",
     "population": "befolkmingsmängd",
     "lanid": "län-ID",
-    "availJobs": "lediga jobb",
-    "jobAds": "jobbannonser",
-    "smahus": "småhus",
-    "flerbostadshus": "flerbostadshus",
-    "ovriga hus": "övriga hus",
-    "specialbostader": "specialbostäder",
+    "availJobs": "antal lediga jobb",
+    "jobAds": "antal jobbannonser",
+    "smahus": "antal småhus",
+    "flerbostadshus": "antal flerbostadshus",
+    "ovriga hus": "antal övriga hus",
+    "specialbostader": "antal specialbostäder",
     "disponibelInkomst": "inkomst",
-    "rape": "våldtäkt",
+    "rape": "antal våldtäkter",
     "fysiskvaldkvinnor": "våld mot kvinnor",
     "fysiskvaldman": "våld mot män",
-    "enskildForskolaArbetare": "privata förskollärare",
+    "enskildForskolaArbetare": "antal privata förskollärare",
     "enskildForskolaArbetarePerBarn": "antal barn per privat förskollärare",
-    "kommunForskolaArbetare": "kommunala förskollärare",
+    "kommunForskolaArbetare": "antal kommunala förskollärare",
     "kommunForskolaArbetarePerBarn": "antal barn per kommunal förskollärare",
-    "psyk_psykosomatiska": "psykiska problem",
-    "psyk_nedstamdhet": "depression",
+    "psyk_psykosomatiska": "förekomst av psykiska problem",
+    "psyk_nedstamdhet": "förekomst av depression",
     "psyk_koncentration": "koncentrationssvårigheter",
-    "suicidewomen": "självmord bland kvinnor",
-    "suicidemen": "självmord bland män",
+    "suicidewomen": "antal självmord bland kvinnor",
+    "suicidemen": "antal självmord bland män",
     "suicidetotal": "antal självmord",
     "utb_pre_gymnasium_women": "förgymnasial utbildning bland kvinnor",
     "utb_pre_gymnasium_men": "förgymnasial utbildning bland män",
@@ -64,12 +64,12 @@ function capitalizeFirstLetter(string) {
 //Hög, stor, massiv
 function getDirWord1(up) {
     if (up) {
-        var words = ["stor", "massiv"];
+        var words = ["stor", "massiv", "ändlöst", "förhöjd", "våldsam"];
         var index = Math.floor(Math.random() * words.length);
         return words[index];
     }
     else {
-        var words = ["låg", "minimal"];
+        var words = ["låg", "minimal", "liten", "klen"];
         var index = Math.floor(Math.random() * words.length);
         return words[index];
     }
@@ -78,16 +78,30 @@ function getDirWord1(up) {
 //Stiger, ökar, sjunker, minskar 
 function getDirWord2(up) {
     if (up) {
-        var words = ["stiger", "ökar", "rekordhögt"];
+        var words = ["stiger", "ökar", "rekordhögt", "historiskt höga nivåer"];
         var index = Math.floor(Math.random() * words.length);
         return words[index];
     }
     else {
-        var words = ["sjunker", "minskar", "på bottennivå"];
+        var words = ["sjunker", "minskar", "på bottennivå", "historiskt låga nivåer"];
         var index = Math.floor(Math.random() * words.length);
         return words[index];
     }
 }
+
+//Stigande, ökande, sjunkande, minskande
+// function getDirWord3(up) {
+//     if (up) {
+//         var words = ["stigande", "ökande"];
+//         var index = Math.floor(Math.random() * words.length);
+//         return words[index];
+//     }
+//     else {
+//         var words = ["sjunkande", "minskande", "all-time-low"];
+//         var index = Math.floor(Math.random() * words.length);
+//         return words[index];
+//     }
+// }
 
 //De två högsta värdena för denna kommun kommer in här.
 function generateSentence(param1, param2, val1, val2, geo, p95_1, p50_1, p95_2, p50_2) {
@@ -106,13 +120,19 @@ function generateSentence(param1, param2, val1, val2, geo, p95_1, p50_1, p95_2, 
     var f5 = "";
 
     if ((up1 && up2) || (!up1 && !up2)) {
-        f1 = capitalizeFirstLetter(par1) + " och  " + par2 + " " + getDirWord2(up1) + " i " + geo;
+        f1 = capitalizeFirstLetter(par1) + " och " + par2 + " " + getDirWord2(up1) + " i " + geo;
+        f2 = capitalizeFirstLetter(getDirWord1(up1)) + " " + par1 + " och " + getDirWord1(up2) + " " + par2 + " i " + geo;
+        f3 = capitalizeFirstLetter(par1) + " " + getDirWord2(up1) + " i " + geo;
     }
     else if (up1 && !up2) {
         f1 = capitalizeFirstLetter(par1) + " " + getDirWord2(up1) + " medan " + par2 + " " + getDirWord2(up2) + " i " + geo;
+        f2 = capitalizeFirstLetter(getDirWord1(up1)) + " " + par1 + " och " + getDirWord1(up2) + " " + par2 + " i " + geo;
+        f3 = capitalizeFirstLetter(par1) + " " + getDirWord2(up1) + " i " + geo;
     }
     else if (up2 && !up1) {
         f1 = capitalizeFirstLetter(par1) + " " + getDirWord2(up1) + " medan " + par2 + " " + getDirWord2(up2) + " i " + geo;
+        f2 = capitalizeFirstLetter(getDirWord1(up1)) + " " + par1 + " och " + getDirWord1(up2) + " " + par2 + " i " + geo;
+        f3 = capitalizeFirstLetter(par1) + " " + getDirWord2(up1) + " i " + geo;
     }
 
     // f2 = capitalizeFirstLetter(param2)+"  "+getDirWord(up)
@@ -128,10 +148,8 @@ function generateSentence(param1, param2, val1, val2, geo, p95_1, p50_1, p95_2, 
     // }
 
     sentences.push(f1);
-    // sentences.push(f2);
-    // sentences.push(f3);
-    // sentences.push(f4);
-    // sentences.push(f5);
+    sentences.push(f2);
+    sentences.push(f3);
     return sentences;
 }
 
@@ -158,7 +176,8 @@ function main() {
     randVal2 = Math.floor(Math.random() * 100);
 
     var res = generateSentence(randParam1, randParam2, randVal1, randVal2, geo, p95_1, p50_1, p95_2, p50_2);
-    console.log(res);
+    var indexRes = Math.floor(Math.random() * res.length);
+    console.log(res[indexRes]);
 }
 
 main();
